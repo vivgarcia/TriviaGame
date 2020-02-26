e730171cebf2119fe0664e3f37234cad823e6674
vivgarcia <viv.garcia88@gmail.com>
2020-02-26 12:34:28 -0600

    additional commit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,62 @@
-$(".startButton").on("click", function(){
-    alert("hi");
-});
+$(document).ready(function(){
+//define variables
+var timeLeft = 16;
 
+var triviaQuestions = [
+    {
+        question: "What state is Parks and Rec based in?",
+        options: [
+            "Indiana", 
+            "Texas", 
+            "Wisconsin", 
+            "California"
+        ],
+        correctAnswer: "0"
+    },
+    {
+        question: "What is the name of Ron Swanson's saxophone playing alter-ego?",
+        options: [
+            "Kenny G", 
+            "Pablo Johnson", 
+            "Duke Silver", 
+            "Sammy Tunes"
+        ],
+        correctAnswer: "2"
+    },
+    {
+        question: "What was April Ludgate's job position at the Pawnee Parks Department initially?",
+        options: [
+            "Janitor", 
+            "College Intern", 
+            "Rapid Animal Extermination Crew", 
+            "Secretary"
+        ],
+        correctAnswer: "1"
+    },
+    {
+        question: "What is the name of the rival town, that Leslie Knope was born in?",
+        options: [
+            "Pawnee", 
+            "Muncie", 
+            "Eagleton", 
+            "Indianapolis"
+        ],
+        correctAnswer: "2"
+    },
+    {
+        question: "Why was Ben Wyatt famous for before moving to Pawnee?",
+        options: [
+            "State Auditor", 
+            "Creating Cones of Dunshire", 
+            "Children's TV Host", 
+            "Kid Mayor"
+        ],
+        correctAnswer: "3"
+    }
+    ]
+
+    $(".startButton").on("click", function(){
+        $(".main-content").text(triviaQuestions[1].question)
+    });
+
+});
